4600ded80d62d8b6833a23c32e9a076adb054582
renatoluluciano <renatolucianosilva@gmail.com>
2024-10-15 15:04:47 -0300

    Removendo projetos sem uso

--- workspace-spring-tool-suite-4-4.25.0.RELEASE/cursospring/src/main/java/com/projetocurso/cursospring/pizza/Tamanho.java
+++
@@ -1,9 +0,0 @@
-package com.projetocurso.cursospring.pizza;
-
-public enum Tamanho {
-	
-	GRANDE,
-	MEDIA,
-	PEQUENA
-
-}
--- workspace-spring-tool-suite-4-4.25.0.RELEASE/projeto-aula/target/classes/import.sql
+++
@@ -1,6 +0,0 @@
-insert into convidado (id, nome, quantidadeAcompanhante)
-values (1, 'Pedro', 2);
-insert into convidado (id, nome, quantidadeAcompanhante)
-values (2, 'Maria', 3);
-insert into convidado (id, nome, quantidadeAcompanhante)
-values (3, 'Ricardo', 1);
